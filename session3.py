93f70530da7902bce3d09332f658327eaf332803
alinazaqat <alinazaqatali8@gmail.com>
2024-06-03 18:19:29 +1000

    Update session3.py

--- session3.py
+++ session3.py
@@ -1,1 +1,6 @@
 # Topic: The if-else Statement
+weather =  input( "what's the weather like today (cloudy, foggy)? ")    
+if weather== "cloudy":
+  print("stay inside its going to rain soon!")
+else:
+  print("Dont drive its foggy with 3% visibility!")
